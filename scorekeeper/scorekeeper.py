6474af025eb6bc75a544a0e13cd6c7f853b17659
Daniel Hosterman <daniel.hosterman@gmail.com>
2016-09-27 05:07:18 +0000

    Initial logic and beginning tests

--- scorekeeper/scorekeeper.py
+++ scorekeeper/scorekeeper.py
@@ -1,1 +1,68 @@
 # -*- coding: utf-8 -*-
+
+import datetime
+import pkg_resources
+import shelve
+
+
+class _Borg(object):
+    _shared_state = {}
+
+    def __init__(self):
+        self.__dict__ = _Borg._shared_state.setdefault(type(self), {})
+
+
+class Scorekeeper(_Borg):
+    def __init__(self):
+        _Borg.__init__(self)
+
+    def __call__(self, score, threshold=100, decay=None, callback=None):
+        self.score = self._get_score()[1]
+        if decay:
+            self._decay(decay)
+        self.score += score
+        if self.score > threshold:
+            self.reset_score()
+            self.default_callback() if not callback else callback()
+        self._set_score()
+
+    def default_callback(self):
+        raise ScorekeeperError("Score has been exceeded!")
+
+    def reset_score(self):
+        self.score = 0
+
+    def seconds_since_last_score(self):
+        return 10
+
+    def _get_score(self):
+        with shelve.open(self._shelve_path()) as db:
+            key = type(self).__name__
+            result = db.get(key) or {}
+        return result.get("datetime"), result.get("score", 0)
+
+    def _set_score(self):
+        with shelve.open(self._shelve_path()) as db:
+            key = type(self).__name__
+            db[key] = {"datetime": datetime.datetime.now(), "score": self.score}
+
+    def _shelve_path(self):
+        return pkg_resources.resource_filename('scorekeeper', 'data/scores')
+
+
+    def _decay(self, rate):
+        time = self._seconds_since_last_score()
+        decay = int(time / rate)
+        self.score =  max(0, self.score - decay)
+
+    def _seconds_since_last_score():
+        return (datetime.datetime.now() - _get_score()[0]).seconds
+
+class ScorekeeperError(Exception):
+    pass
+
+
+class Singleton(Scorekeeper):
+    def default_callback(self):
+        print("Yay!")
+
--- tests/test_scorekeeper.py
+++ tests/test_scorekeeper.py
@@ -10,20 +10,26 @@ Tests for `scorekeeper` module.
 
 import pytest
 
+from scorekeeper.scorekeeper import Scorekeeper
 
-from scorekeeper import scorekeeper
+tmp_path = None
 
 
-class TestScorekeeper(object):
+@pytest.fixture
+def scorekeeper_obj(tmpdir):
+    class ScorekeeperObj(Scorekeeper):
+        def _shelve_path(self):
+            global tmp_path
+            if not tmp_path:
+                tmp_path = str(tmpdir.mkdir("data").join("/scores"))
 
-    @classmethod
-    def setup_class(cls):
-        pass
+            return tmp_path
 
-    def test_something(self):
-        pass
+    yield ScorekeeperObj()
+    tmp_path = None
 
-    @classmethod
-    def teardown_class(cls):
-        pass
 
+@pytest.mark.parametrize("points, expected", [(10, 10), (20, 30), (30, 60)])
+def test_scorekeeper_increments(scorekeeper_obj, points, expected):
+    scorekeeper_obj(points)
+    assert scorekeeper_obj.score == expected
